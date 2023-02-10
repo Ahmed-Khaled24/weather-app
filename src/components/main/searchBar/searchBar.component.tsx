4dcea3c17aa24548ffb1ac67b8a6543f5de06c46
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-10 23:01:00 +0200

    feat: create searchBar

---
+++ src/components/main/searchBar/searchBar.component.tsx
@@ -0,0 +1,35 @@
+import './searchBar.style.css';
+import searchIcon from '../../../assets/search-icon.svg';
+
+type Props = {
+	setLocation: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const SearchBar = ({ setLocation }: Props) => {
+	let searchInput = '';
+	return (
+		<div
+			className='search-bar'
+			onKeyDown={(e) => {
+				if (e.key === 'Enter') {
+					setLocation(searchInput);
+				}
+			}}
+		>
+			<img
+				src={searchIcon}
+				alt='search-icon'
+				onClick={() => setLocation(searchInput)}
+			/>
+			<input
+				type='text'
+				placeholder='Type something...'
+				onChange={(e) => {
+					searchInput = e.target.value;
+				}}
+			/>
+		</div>
+	);
+};
+
+export default SearchBar;
