2daa313e6bd5e736fbaf05cfd7a8b3a8fe24a41e
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-11 19:31:17 +0200

    feat: change main component
    change wind speed to feels like and
    rain chance to clouds

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -2,49 +2,47 @@ import './main.style.css';
 import { WeatherStatusData } from '../../types/weather.types';
 import MainCard from './mainCard/mainCard.component';
 import SmallCard from './smallCard/smallCard.component';
-import rainIcon from '../../assets/water-drop-icon.svg';
+import thermostatIcon from '../../assets/thermostat-icon.svg'
 import humidityIcon from '../../assets/humidity-icon.svg';
 import pressureIcon from '../../assets/pressure-icon.svg';
-import windIcon from '../../assets/wind-icon.svg';
+import cloudIcon from '../../assets/cloud-icon.svg'
 import SearchBar from './searchBar/searchBar.component';
 
 
-
-
-interface Props {
+type Props = {
 	weatherStatus: WeatherStatusData;
 	setLocation: React.Dispatch<React.SetStateAction<string>>
 }
 
 const Main = ({ weatherStatus, setLocation }: Props) => {
-	const { windSpeed, pressure, rainChance, humidity } = weatherStatus;
+	const {pressure, humidity, feelsLike, clouds } = weatherStatus;
 	return (
 		<div className='main'>
 			<SearchBar setLocation={setLocation}/>
 			<MainCard weatherStatus={weatherStatus} background='' />
 			<div className='small-cards'>
 				<SmallCard
-					title={'Wind'}
-					subtitle={'Today wind speed'}
-					data={`${windSpeed}Km/h`}
-					imagePath={windIcon}
+					title={'Feels Like'}
+					subtitle={'Today it feels like'}
+					data={`${feelsLike}°`}
+					imagePath={thermostatIcon}
 				/>
 				<SmallCard
-					title={'Rain Chance'}
-					subtitle={'Today rain chance'}
-					data={`${rainChance}%`}
-					imagePath={rainIcon}
+					title={'Clouds'}
+					subtitle={'Today clouds'}
+					data={`${clouds}%`}
+					imagePath={cloudIcon}
 				/>
 				<SmallCard
 					title={'Pressure'}
 					subtitle={'Today pressure'}
-					data={`${pressure}hpa`}
+					data={`${pressure} hpa`}
 					imagePath={pressureIcon}
 				/>
 				<SmallCard
 					title={'Humidity'}
-					subtitle={'Today humidity index'}
-					data={`${humidity}`}
+					subtitle={'Today humidity'}
+					data={`${humidity}%`}
 					imagePath={humidityIcon}
 				/>
 			</div>
