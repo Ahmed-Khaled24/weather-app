08a849fd5f4545b207e407d7c5e9ce4fd5bfbe47
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-11 20:25:11 +0200

    feat: add react router to the app

--- src/components/sidePanel/sidePanel.component.tsx
+++ src/components/sidePanel/sidePanel.component.tsx
@@ -1,18 +1,28 @@
 import './sidePanel.style.css';
+import { Link } from 'react-router-dom';
 
 interface Props {
 	elements: string[];
-	currentRoute: string
+	currentRoute: string;
+	setCurrentRoute: React.Dispatch<React.SetStateAction<string>>,
 }
 
-const SidePanel = ({elements, currentRoute} : Props) => {
+const SidePanel = ({ elements, currentRoute, setCurrentRoute }: Props) => {
 	return (
 		<div className='side-panel'>
 			<p className='logo'> Weather </p>
 			<div className='side-panel-elements'>
-				{elements.map((el) => { 
-					let selected = currentRoute === el ? 'selected': '';
-					return <p className={`side-panel-element ${el} ${selected}`}> {el} </p>
+				{elements.map((el) => {
+					let selected = currentRoute.toLowerCase() === el.toLowerCase() ? 'selected' : '';
+					return (
+						<Link
+							className={`side-panel-element ${selected}`}
+							to={el.toLowerCase()}
+							onClick={() => setCurrentRoute(el.toLowerCase())}
+						>	
+							{el}
+						</Link>
+					);
 				})}
 			</div>
 		</div>
