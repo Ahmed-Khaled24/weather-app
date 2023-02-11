78662a9ca493133f2a07ed34659f4eabfb7184b4
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-02-11 19:41:14 +0200

    feat: change sidePanel component
    render selected tab according to the route

--- src/components/sidePanel/sidePanel.component.tsx
+++ src/components/sidePanel/sidePanel.component.tsx
@@ -2,16 +2,18 @@ import './sidePanel.style.css';
 
 interface Props {
 	elements: string[];
+	currentRoute: string
 }
 
-const SidePanel = ({elements} : Props) => {
+const SidePanel = ({elements, currentRoute} : Props) => {
 	return (
 		<div className='side-panel'>
 			<p className='logo'> Weather </p>
 			<div className='side-panel-elements'>
-				{elements.map((el) => (
-					<p className={`side-panel-element ${el} selected`}> {el} </p>
-				))}
+				{elements.map((el) => { 
+					let selected = currentRoute === el ? 'selected': '';
+					return <p className={`side-panel-element ${el} ${selected}`}> {el} </p>
+				})}
 			</div>
 		</div>
 	);
